56fe32c47d968873449dbf52f568c8c4b9e291eb
hackcharms <zaa78692@gmail.com>
2024-05-01 14:02:45 +0530

    improved const structure

---
+++ src/constants/experiences.ts
@@ -0,0 +1,31 @@
+import { credilio, tcw } from "../assets";
+
+export const experiences = [
+  {
+    title: "Software Engineer (Frontend)",
+    company_name: "Credilio Financial Technologies",
+    icon: credilio,
+    iconBg: "#c491ef",
+    date: "Dec 2022 - Present",
+    points: [
+      "Refactored and modularized the project structure using composable components and other approaches to improve accessibility and maintainability.",
+      "Optimized page load times by leveraging a deep understanding of lifecycle hooks, rendering, asynchronous components, virtual scrolling, debouncing, throttles, etc.",
+      "Implemented the access based on roles and policy to restrict unauthorized access.",
+      "Dynamic theme support to adopt and blend with partner platforms and multiple native supports like image, text sharing, clipboard, etc.",
+      "Assisted the partners integration of our web page in their application and led a group of 3-4 developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "TheCodeWork",
+    icon: tcw,
+    iconBg: "#c491ef",
+    date: "May 2021 - Nov 2022",
+    points: [
+      "Developing and maintaining web applications using Larvel, Vuejs and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
---
+++ src/constants/technologies.ts
@@ -0,0 +1,95 @@
+import {
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  docker,
+  threejs,
+  vuejs,
+  nuxtjs,
+  pinia,
+  vuetify,
+  laravel,
+  mysql,
+} from "../assets";
+
+export const technologies = [
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Vue JS",
+    icon: vuejs,
+  },
+  {
+    name: "Nuxt JS",
+    icon: nuxtjs,
+  },
+  {
+    name: "Pinia",
+    icon: pinia,
+  },
+  {
+    name: "Vuetify",
+    icon: vuetify,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Laravel",
+    icon: laravel,
+  },
+  {
+    name: "MySql",
+    icon: mysql,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
